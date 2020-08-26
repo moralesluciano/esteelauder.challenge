5174b9fc6a685cb19a2f41cadddb7291d8218bcf
Luciano Morales <luciano.morales@wellsky.com>
2020-08-26 20:34:45 -0300

    ability to create multiple transactions

--- my_app/lib/Transactions.pm
+++ my_app/lib/Transactions.pm
@@ -6,18 +6,15 @@ use Mojo::Base 'Mojolicious', -signatures;
 use Scalar::Util qw(looks_like_number);
 use POSIX qw(strftime);
 
-# array to store all transactions
-my @transactions = ();
-
 # store the balance
 my $balance = 0;
 
-# handle several transactions at the same time with concurrent access
-my $lock_transaction = 0;
-
 sub new {
     my $class = shift;
-    my $self = {};
+
+    my $self = {
+        transactions => []
+    };
 
     bless $self, $class;
 
@@ -27,49 +24,40 @@ sub new {
 sub get_all {
     my ($self) = @_;
 
-    return @transactions;
+    return @{$self->{transactions}};
 }
 
 sub get_transaction_by_id {
     my ($self, $id) = @_;
 
-    my $transactionFound = ''; 
-    foreach my $transaction (@transactions) { 
+    my $transactionFound = '';
+    foreach my $transaction (@{$self->{transactions}}) {
         if( $transaction->{"id"} eq $id ) {
             $transactionFound = $transaction;
             last;
         };
     }
 
-    return $transactionFound; 
+    return $transactionFound;
 }
 
 sub add_transaction {
     my ($self, $params) = @_;
 
-    if($lock_transaction) {
-        return;
-    }
-    $self->lock_transaction();
-
     if(! $self->looks_params_good($params)) {
-        $self->unlock_transaction();
         return ("400", "BAD REQUEST")
     }
 
     if(! $self->can_add_transaction($params)) {
-        $self->unlock_transaction();
         return ("400", "CAN NOT PROCESS THE OPERATION - BALANCE IS NOT ENOUGH")
     }
 
-    push(@transactions, {
+    push(@{$self->{transactions}}, {
         id            => "uuid-".int(rand(1000)), # for some reason i am not able to use UUID::Tiny to generate the uuid
         type          => $params->{"type"},
         amount        => $params->{"amount"},
-        effectiveDate => strftime("%FT%T", localtime(time)) . sprintf ".%03d" # do not want to include DateTime just for that   
+        effectiveDate => strftime("%FT%T", localtime(time)) . sprintf ".%03d" # do not want to include DateTime just for that
     });
-
-    $self->unlock_transaction();
 
     return ("200", "");
 
@@ -78,7 +66,7 @@ sub add_transaction {
 sub looks_params_good {
     my ($self, $params) = @_;
 
-    return 
+    return
         ($params->{"type"} eq "credit" || $params->{"type"} eq "debit")
      && (looks_like_number($params->{"amount"}));
 
@@ -99,20 +87,10 @@ sub can_add_transaction {
             $can_add_transaction = 1;
         } else {
             $can_add_transaction = 0;
-        } 
+        }
     }
 
-    return $can_add_transaction; 
-}
-
-sub lock_transaction {
-    my ($self) = shift;
-    $lock_transaction++;
-}
-
-sub unlock_transaction {
-    my ($self) = shift;
-    $lock_transaction--;
+    return $can_add_transaction;
 }
 
 1;
